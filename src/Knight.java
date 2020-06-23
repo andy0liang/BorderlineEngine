d4f7345ecb8534683cad80de7a2dd72f276a4cd2
Andy Liang <andy0liang@gmail.com>
2020-06-23 04:47:26 -0700

    half of knight

--- src/Knight.java
+++ src/Knight.java
@@ -2,8 +2,33 @@ import java.util.*;
 public class Knight{
     public static ArrayDeque<Board> possibleMoves(Board board, int startloc) {
         ArrayDeque<Board> arr = new ArrayDeque<>();
+        int row = startloc/8;
+        int column = startloc%8;
+        if(board.squares[startloc]>0){ //white
 
-        
+            if(row<6){
+                if(column!=7){
+                    arr.add(new Board(board, 1, startloc, startloc+17));
+                }
+                if(column!=0){
+                    arr.add(new Board(board, 1, startloc, startloc+15));
+                }
+            }
+            if(row>1){
+                if(column!=7){
+                    arr.add(new Board(board, 1, startloc, startloc-15));
+                }
+                if(column!=0){
+                    arr.add(new Board(board, 1, startloc, startloc-17));
+                }
+            }
+
+
+        }
+        else{ //black
+
+
+        }
 
         return arr;
     }
