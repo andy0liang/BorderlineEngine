390e79aa2f47a30a3d4da702b62341b441b024b3
Andy Liang <andy0liang@gmail.com>
2020-06-24 00:25:54 -0700

    added queen

--- src/Queen.java
+++ src/Queen.java
@@ -1,8 +1,10 @@
 import java.util.*;
 public class Queen{
-    public static ArrayDeque<Board> possibleMoves(Board board, int startloc) {
-        ArrayDeque<Board> arr = new ArrayDeque<>();
+    public static ArrayList<Board> possibleMoves(Board board, int startloc) {
+        ArrayList<Board> arr = new ArrayList<>();
 
+        Knight.possibleMoves(board,startloc).forEach(n->arr.add(n));
+        Rook.possibleMoves(board,startloc).forEach(n->arr.add(n));
 
         return arr;
     }
