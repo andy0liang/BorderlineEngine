2c347cba92707480aafff31b671a14c664ca0120
Andy Liang <andy0liang@gmail.com>
2020-07-11 03:01:32 -0700

    updated king

--- src/King.java
+++ src/King.java
@@ -2,6 +2,24 @@ import java.util.*;
 public class King{
     public static ArrayList<Board> possibleMoves(Board board, int startloc) {
         ArrayList<Board> arr = new ArrayList<>();
+        int row = startloc/8;
+        int column = startloc%8;
+        Board temp;
+        if(board.squares[startloc]>0){
+
+            if(startloc+8<64){
+                temp = new Board(board, 1, startloc, startloc+8);
+            }
+            if(startloc+9<64){
+                temp = new Board(board, 1, startloc, startloc+9);
+
+            }
+
+        }
+        else{
+
+
+        }
 
 
         return arr;
