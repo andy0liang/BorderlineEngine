f7405be9ed1aa7c5f9f56bf5fe15b892621826ad
Andy Liang <andy0liang@gmail.com>
2020-07-11 03:10:00 -0700

    finished king defendlist

--- src/King.java
+++ src/King.java
@@ -12,7 +12,6 @@ public class King{
             }
             if(startloc+9<64){
                 temp = new Board(board, 1, startloc, startloc+9);
-
             }
 
         }
@@ -23,5 +22,39 @@ public class King{
 
 
         return arr;
+    }
+    public static ArrayList<Integer> defendList (Board board, int startloc){
+        ArrayList <Integer> arr = new ArrayList <>();
+        int row = startloc/8;
+        int column = startloc%8;
+
+        if(startloc+8<64){
+            arr.add(startloc+8);
+        }
+        if(startloc+9<64 && column!=7){
+            arr.add(startloc+9);
+        }
+        if(startloc+7<64 && column!=0){
+            arr.add(startloc+7);
+        }
+        if(column!=7){
+            arr.add(startloc+1);
+        }
+        if(column!=0){
+            arr.add(startloc-1);
+        }
+        if(startloc-8>=0){
+            arr.add(startloc-8);
+        }
+        if(startloc-9>=0 && column!=0){
+            arr.add(startloc-9);
+        }
+        if(startloc-7>=0 && column!=7){
+            arr.add(startloc-7);
+        }
+
+        return arr;
+
+
     }
 }
